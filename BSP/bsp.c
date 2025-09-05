6607bd35eed6799e1d542f50f23c4a0be9f8d048
sunwen <sunwen.me@qq.com>
2025-09-05 21:38:26 +0800

    Initial commit

---
+++ BSP/bsp.c
@@ -0,0 +1,35 @@
+#include "bsp.h"
+
+// LED显示当前运行状态，每10毫秒调用一次，LED灯每200毫秒闪烁一次。
+// The LED displays the current operating status, which is invoked every 10 milliseconds, and the LED blinks every 200 milliseconds.  
+static void Bsp_Led_Show_State_Handle(void)
+{
+	static uint8_t led_count = 0;
+	led_count++;
+	if (led_count > 20)
+	{
+		led_count = 0;
+	}
+}
+
+
+// The peripheral device is initialized  外设设备初始化
+void Bsp_Init(void)
+{
+
+	USART1_Init();
+	Motor_Init();
+	Encoder_Init();
+}
+
+
+// main.c中循环调用此函数，避免多次修改main.c文件。
+// This function is called in a loop in main.c to avoid multiple modifications to the main.c file
+void Bsp_Loop(void)
+{
+
+	SBUS_Handle();
+
+	// The buzzer automatically shuts down when times out   蜂鸣器超时自动关闭
+	HAL_Delay(10);
+}
---
+++ BSP/bsp_akm.h
@@ -0,0 +1,8 @@
+//
+// Created by sw on 2025/9/5.
+//
+
+#ifndef STM32C_BSP_AKM_H
+#define STM32C_BSP_AKM_H
+
+#endif //STM32C_BSP_AKM_H
